b829e9f03109ab16fc1525158b7fc1a1a8a66e86
Dennis Hübner <dennis.huebner89@gmail.com>
2017-03-17 15:03:49 +0100

    implemented beginning of something, but need a working debugger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
 use std::io;
+use std::vec::Vec;
+
+/**
+ *
+ * - struct for all question types
+ * - input routine
+ * - hash set as data structure
+ * - export function
+ *
+ */
+
+struct Answer {
+    title: String,
+    points: i32,
+}
+
+struct Question {
+    question_type: QuestionType,
+    title: String,
+    answers: Vec<Answer>,
+}
+
+enum QuestionType {
+    SingleChoice,
+    MultipleChoice,
+    Essay,
+    TrueFalse,
+    Matching,
+    ShortAnswer,
+    NumericalResponse,
+}
 
 fn main() {
-    println!("Hello, world!");
+    println!("Bitte Fragetyp eingeben: [1-7], 0 for help");
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-            println!("{}", input);
-        }
+        Ok(n) => {}
         Err(error) => println!("error: {}", error),
     }
-    new_question("single_choice", &input);
+
+    if input == "1" {
+        println!("SingleChoice");
+    }
+    println!("{}", input);
 }
 
 fn new_question(question_type: &str, name: &str) {
